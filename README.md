764f978e62dae25dc382a6cd18fbec853c6cfef0
i.iliev <i.t.iliev@armf.bg>
2022-06-16 21:46:25 +0300

    name, price, color and size to json

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Example-scraper
 A scrapy/selenium scraper of https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99
 
+# build
 python -m venv venv
 .\venv\Scripts\activate
 pip install requirements.txt
+
+# crawl
+cd .\mango_shop\
+scrapy crawl midi_satin_skirt -o product.json
---
+++ mango_shop/mango_shop/spiders/midi_satin_skirt.py
@@ -0,0 +1,37 @@
+import scrapy
+from selenium import webdriver
+import time
+import json
+
+class MidiSatinSkirtSpider(scrapy.Spider):
+    name = 'midi_satin_skirt'
+    allowed_domains = ['shop.mango.com']
+    start_urls = [
+        'https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99',
+    ]
+
+    
+
+    def parse(self, response):
+        self.driver = webdriver.Firefox()
+        self.driver.get(response.request.url)
+        self.driver.find_element_by_id('onetrust-accept-btn-handler').click()
+        time.sleep(1)
+        self.driver.find_element_by_class_name('icon.closeModal.icon__close.desktop.confirmacionPais').click()
+        
+        name = self.driver.find_element_by_class_name('product-name').text
+        price = float(self.driver.find_element_by_class_name('product-sale').text[1:])
+        color = self.driver.find_element_by_class_name('colors-info-name').text
+        size_combo_box = self.driver.find_element_by_class_name('selector-trigger')
+        if size_combo_box.get_attribute("aria-selected") == 'false':
+            size = 'Not available'
+        else:
+            size = size_combo_box.text
+        
+        yield {
+            'name': name,
+            'price': price,
+            'color': color,
+            'size': size
+        }
+        
