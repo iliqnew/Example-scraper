d70cd0bdf598f1fcaad37cd80edee5102b4dc97d
Iliyan <iliqanew@gmail.com>
2022-06-17 09:26:29 +0300

    list of sizes

--- mango_shop/mango_shop/spiders/midi_satin_skirt.py
+++ mango_shop/mango_shop/spiders/midi_satin_skirt.py
@@ -15,6 +15,7 @@ class MidiSatinSkirtSpider(scrapy.Spider):
     def parse(self, response):
         self.driver = webdriver.Firefox()
         self.driver.get(response.request.url)
+        time.sleep(1)
         self.driver.find_element_by_id('onetrust-accept-btn-handler').click()
         time.sleep(1.5)
         self.driver.find_element_by_class_name('icon.closeModal.icon__close.desktop.confirmacionPais').click()
@@ -22,16 +23,14 @@ class MidiSatinSkirtSpider(scrapy.Spider):
         name = self.driver.find_element_by_class_name('product-name').text
         price = float(self.driver.find_element_by_class_name('product-sale').text[1:])
         color = self.driver.find_element_by_class_name('colors-info-name').text
-        size_combo_box = self.driver.find_element_by_class_name('selector-trigger')
-        if size_combo_box.get_attribute("aria-selected") == 'false':
-            size = 'Not available'
-        else:
-            size = size_combo_box.text
+        size_selector_list = self.driver.find_element_by_class_name('selector-list')
+        size_choices = size_selector_list.find_elements_by_xpath('span')
+        sizes = [e.get_attribute("data-size") for e in size_choices]
         
         yield {
             'name': name,
             'price': price,
             'color': color,
-            'size': size
+            'size': sizes
         }
         
